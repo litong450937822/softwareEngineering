3391fee4bf619a89649183330d6fa0db76a2672a
litong450937822 <li15929939221@163.com>
2019-03-18 09:53:54 +0800

    2019/03/17

--- connect/software.sql
+++ connect/software.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2019-03-12 23:01:00
+Date: 2019-03-18 09:53:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -100,7 +100,7 @@ CREATE TABLE `discass_t` (
 -- ----------------------------
 -- Records of discass_t
 -- ----------------------------
-INSERT INTO `discass_t` VALUES ('1', '测试讨论题目1', '1', '2019/03/11 10:00:56', '10', '2019/03/03 01:02:28');
+INSERT INTO `discass_t` VALUES ('1', '测试讨论题目1', '1', '2019/03/11 10:00:56', '13', '2019/03/03 01:02:28');
 
 -- ----------------------------
 -- Table structure for question_q
@@ -118,7 +118,7 @@ CREATE TABLE `question_q` (
 -- ----------------------------
 -- Records of question_q
 -- ----------------------------
-INSERT INTO `question_q` VALUES ('2', '21412421412', '1');
+INSERT INTO `question_q` VALUES ('2', '测试问卷2问题1', '1');
 INSERT INTO `question_q` VALUES ('2', '21412412421', '2');
 INSERT INTO `question_q` VALUES ('2', '214124124124', '3');
 INSERT INTO `question_q` VALUES ('2', '21412421412412', '4');
@@ -134,6 +134,10 @@ INSERT INTO `question_q` VALUES ('4', '346435345', '3');
 INSERT INTO `question_q` VALUES ('4', '34543634', '4');
 INSERT INTO `question_q` VALUES ('4', '3463463', '5');
 INSERT INTO `question_q` VALUES ('4', '34634636', '6');
+INSERT INTO `question_q` VALUES ('5', '测试问卷1问题1', '1');
+INSERT INTO `question_q` VALUES ('5', '21412412421', '2');
+INSERT INTO `question_q` VALUES ('5', '214124124124', '3');
+INSERT INTO `question_q` VALUES ('5', '21412421412412', '4');
 
 -- ----------------------------
 -- Table structure for question_s
@@ -146,7 +150,7 @@ CREATE TABLE `question_s` (
   `submitTime` varchar(19) NOT NULL,
   PRIMARY KEY (`qtid`,`sid`) USING BTREE,
   KEY `student_q` (`sid`) USING BTREE,
-  CONSTRAINT `question` FOREIGN KEY (`qtid`) REFERENCES `question_t` (`qtid`) ON DELETE CASCADE,
+  CONSTRAINT `question` FOREIGN KEY (`qtid`) REFERENCES `question_t` (`qtid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `student_q` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
@@ -169,14 +173,15 @@ CREATE TABLE `question_t` (
   PRIMARY KEY (`qtid`,`cid`),
   KEY `course_q` (`cid`) USING BTREE,
   CONSTRAINT `course_q` FOREIGN KEY (`cid`) REFERENCES `course` (`cid`) ON DELETE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of question_t
 -- ----------------------------
-INSERT INTO `question_t` VALUES ('2', '12412412214', '1', '2019/03/05 09:45:23', '2019/03/30 00:00:00');
+INSERT INTO `question_t` VALUES ('2', '测试问卷2', '1', '2019/03/05 09:45:23', '2019/03/30 00:00:00');
 INSERT INTO `question_t` VALUES ('3', '1242142142141245245745', '1', '2019/03/05 09:46:34', '2020/04/05 00:00:00');
 INSERT INTO `question_t` VALUES ('4', '1241245435', '1', '2019/03/10 03:57:11', '2019/03/22 00:00:00');
+INSERT INTO `question_t` VALUES ('5', '测试问卷1', '1', '2019/03/05 09:45:23', '2019/03/30 00:00:00');
 
 -- ----------------------------
 -- Table structure for student
@@ -190,8 +195,9 @@ CREATE TABLE `student` (
   `number` int(11) NOT NULL,
   `sex` varchar(1) DEFAULT NULL,
   `studyTime` int(255) NOT NULL DEFAULT '0',
-  `profilePhoto` varchar(255) DEFAULT NULL,
+  `profilePhoto` varchar(255) DEFAULT 'default.jpg',
   PRIMARY KEY (`sid`,`number`),
+  UNIQUE KEY `studentNumber` (`number`),
   KEY `class1` (`clid`),
   CONSTRAINT `class1` FOREIGN KEY (`clid`) REFERENCES `class` (`clid`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
@@ -199,8 +205,8 @@ CREATE TABLE `student` (
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES ('1', '测试学生1', '1', '123456', '1000001', 'W', '0', '');
-INSERT INTO `student` VALUES ('2', '测试学生2', '1', '123456', '1000002', null, '0', '');
+INSERT INTO `student` VALUES ('1', '测试学生1', '1', '123456', '1000001', 'W', '0', 'default.jpg');
+INSERT INTO `student` VALUES ('2', '测试学生2', '1', '123456', '1000002', 'M', '0', 'default.jpg');
 
 -- ----------------------------
 -- Table structure for teacher
@@ -248,8 +254,8 @@ INSERT INTO `test_q` VALUES ('13', '2', '测试问题2', '2222222222', '3333', '
 INSERT INTO `test_q` VALUES ('13', '3', '测试问题3', '111', '2222222', '333', '333333', 'B');
 INSERT INTO `test_q` VALUES ('13', '4', '测试问题4', '1111111111111111', '2222', '3333333', '444444444444', 'A');
 INSERT INTO `test_q` VALUES ('13', '5', '测试问题5', '111', '22222222', '333333', '4444', 'B');
-INSERT INTO `test_q` VALUES ('15', '1', '124124', '12412421', '4124', '412421', '214214', 'C');
-INSERT INTO `test_q` VALUES ('15', '2', '124124', '12421421', '2142142', '214124', '214214124', 'C');
+INSERT INTO `test_q` VALUES ('15', '1', '124124', '12421421', '2142142', '214124', '214214124', 'D');
+INSERT INTO `test_q` VALUES ('15', '2', '124124', '12421421421', '2142142', '214124131311331', '214214124', 'C');
 INSERT INTO `test_q` VALUES ('16', '1', '21412', '214212', '1421421', '241', '24', 'C');
 INSERT INTO `test_q` VALUES ('16', '2', '124124', '214124', '21421', '4214214', '2144', 'C');
 INSERT INTO `test_q` VALUES ('16', '3', '2141', '124124124', '21412412', '214214', '124214124', 'C');
@@ -300,7 +306,7 @@ CREATE TABLE `test_t` (
 -- ----------------------------
 INSERT INTO `test_t` VALUES ('12', '测试数据1', '2019/03/03 12:06:31', '2019/07/05 00:00:00', '1');
 INSERT INTO `test_t` VALUES ('13', '测试数据2', '2019/03/11 10:39:24', '2019/06/11 00:00:00', '1');
-INSERT INTO `test_t` VALUES ('15', '1242141', '2019/03/11 10:42:13', '2019/04/06 00:00:00', '1');
+INSERT INTO `test_t` VALUES ('15', '测试测验3', '2019/03/11 10:42:13', '2019/04/06 00:00:00', '1');
 INSERT INTO `test_t` VALUES ('16', '12412421412', '2019/03/11 10:42:36', '2019/04/06 00:00:00', '1');
 
 -- ----------------------------
@@ -315,26 +321,39 @@ CREATE TABLE `time` (
   `startTime` varchar(8) DEFAULT NULL,
   `endTime` varchar(8) DEFAULT NULL,
   `sid` int(11) NOT NULL,
+  `cid` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   KEY `student1` (`sid`),
   CONSTRAINT `student1` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of time
 -- ----------------------------
-INSERT INTO `time` VALUES ('4', '2019/03/11', '586', 'L', '09:35:54', '09:45:40', '1');
-INSERT INTO `time` VALUES ('8', '2019/03/11', '6', 'W', '10:19:47', '10:19:53', '1');
-INSERT INTO `time` VALUES ('9', '2019/03/11', '1840', 'L', '09:49:37', '10:20:17', '1');
-INSERT INTO `time` VALUES ('11', '2019/03/11', '576', 'L', '10:23:38', '10:33:14', '1');
-INSERT INTO `time` VALUES ('13', '2019/03/11', '22', 'L', '10:38:13', '10:38:35', '1');
-INSERT INTO `time` VALUES ('15', '2019/03/11', '8', 'T', '10:43:51', '10:43:59', '1');
-INSERT INTO `time` VALUES ('16', '2019/03/11', '130', 'L', '10:43:49', '10:45:59', '1');
-INSERT INTO `time` VALUES ('17', '2019/03/11', '399', 'L', '10:46:21', '10:53:00', '1');
-INSERT INTO `time` VALUES ('18', '2019/03/11', '55', 'L', '10:55:31', '10:56:26', '1');
-INSERT INTO `time` VALUES ('19', '2019/03/12', '181', 'L', '07:56:00', '07:59:01', '1');
-INSERT INTO `time` VALUES ('20', '2019/03/12', '8251', 'L', '07:59:05', '10:16:36', '1');
-INSERT INTO `time` VALUES ('21', '2019/03/12', '2627', 'L', '22:17:10', '23:00:57', '1');
+INSERT INTO `time` VALUES ('4', '2019/03/11', '586', 'L', '09:35:54', '09:45:40', '1', '1');
+INSERT INTO `time` VALUES ('8', '2019/03/11', '6', 'W', '10:19:47', '10:19:53', '1', '1');
+INSERT INTO `time` VALUES ('9', '2019/03/11', '1840', 'L', '09:49:37', '10:20:17', '1', '1');
+INSERT INTO `time` VALUES ('11', '2019/03/11', '576', 'L', '10:23:38', '10:33:14', '1', '1');
+INSERT INTO `time` VALUES ('13', '2019/03/11', '22', 'L', '10:38:13', '10:38:35', '1', '1');
+INSERT INTO `time` VALUES ('15', '2019/03/11', '8', 'T', '10:43:51', '10:43:59', '1', '1');
+INSERT INTO `time` VALUES ('16', '2019/03/11', '130', 'L', '10:43:49', '10:45:59', '1', '1');
+INSERT INTO `time` VALUES ('17', '2019/03/11', '399', 'L', '10:46:21', '10:53:00', '1', '1');
+INSERT INTO `time` VALUES ('18', '2019/03/11', '55', 'L', '10:55:31', '10:56:26', '1', '1');
+INSERT INTO `time` VALUES ('19', '2019/03/12', '181', 'L', '07:56:00', '07:59:01', '1', '1');
+INSERT INTO `time` VALUES ('20', '2019/03/12', '8251', 'L', '07:59:05', '10:16:36', '1', '1');
+INSERT INTO `time` VALUES ('21', '2019/03/12', '5234', 'L', '22:17:10', '23:44:24', '1', '1');
+INSERT INTO `time` VALUES ('23', '2019/03/13', '15', 'L', '22:48:48', '22:49:03', '1', '1');
+INSERT INTO `time` VALUES ('24', '2019/03/13', '159', 'L', '22:51:53', '22:54:32', '2', '1');
+INSERT INTO `time` VALUES ('25', '2019/03/13', null, 'D', null, null, '2', '1');
+INSERT INTO `time` VALUES ('26', '2019/03/13', null, 'V', null, null, '2', '1');
+INSERT INTO `time` VALUES ('27', '2019/03/13', null, 'Q', null, null, '2', '1');
+INSERT INTO `time` VALUES ('28', '2019/03/14', '70', 'L', '23:33:17', '23:34:27', '1', null);
+INSERT INTO `time` VALUES ('29', '2019/03/14', null, 'O', null, null, '1', '1');
+INSERT INTO `time` VALUES ('30', '2019/03/14', null, 'V', null, null, '1', '1');
+INSERT INTO `time` VALUES ('31', '2019/03/14', null, 'Q', null, null, '1', '1');
+INSERT INTO `time` VALUES ('32', '2019/03/14', null, 'E', null, null, '1', '1');
+INSERT INTO `time` VALUES ('33', '2019/03/14', null, 'O', null, null, '1', '1');
+INSERT INTO `time` VALUES ('34', '2019/03/14', null, 'D', null, null, '1', '1');
 
 -- ----------------------------
 -- Table structure for vote_s
@@ -347,7 +366,7 @@ CREATE TABLE `vote_s` (
   PRIMARY KEY (`vtid`,`sid`,`result`) USING BTREE,
   KEY `student_v` (`sid`) USING BTREE,
   CONSTRAINT `student_v` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`) ON DELETE CASCADE,
-  CONSTRAINT `vtid` FOREIGN KEY (`vtid`) REFERENCES `vote_t` (`vtid`) ON DELETE CASCADE
+  CONSTRAINT `vtid` FOREIGN KEY (`vtid`) REFERENCES `vote_t` (`vtid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
@@ -375,7 +394,7 @@ CREATE TABLE `vote_t` (
 -- ----------------------------
 -- Records of vote_t
 -- ----------------------------
-INSERT INTO `vote_t` VALUES ('1', '1241242154', '12512512;1251252;56756756;567567534', '2019/03/10 03:56:37', '2019/03/30 00:00:00', '1');
+INSERT INTO `vote_t` VALUES ('1', '测试投票1', '测试选项1;12412421;214124;21421412412', '2019/03/10 03:56:37', '2019/03/30 00:00:00', '1');
 
 -- ----------------------------
 -- Table structure for work_s
@@ -422,4 +441,4 @@ CREATE TABLE `work_t` (
 -- ----------------------------
 INSERT INTO `work_t` VALUES ('1', '测试作业1', '1', '测试作业1内容', '2018/11/16 16:00', '2018/11/30 18:00', null);
 INSERT INTO `work_t` VALUES ('2', '测试作业2', '1', '测试作业2内容', '2019/03/08 12:39:24', '2019/07/05 00:00:00', '');
-INSERT INTO `work_t` VALUES ('4', '测试作业3', '1', '测试内容3', '2019/03/11 09:48:02', '2019/07/04 00:00:00', '《PHP程序设计》设计报告.doc');
+INSERT INTO `work_t` VALUES ('4', '测试作业3', '1', '测试内容3update', '2019/03/11 09:48:02', '2019/07/04 00:00:00', '《PHP程序设计》设计报告.doc');
